bdac2cedb936ee3080d0e630ed3c5ae8ba647fae
eunkirosa <eunkirosa@swu.ac.kr>
2025-07-24 15:32:05 +0900

    DB 클래스 추가 (수정,추가 요)

---
+++ GURU2/app/src/main/java/com/example/guru2/UserDBHelper.kt
@@ -0,0 +1,28 @@
+package com.example.guru2
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class UserDBHelper {
+    class UserDBHelper(context: Context) :
+        SQLiteOpenHelper(context, "UserDB", null, 1) {
+
+        override fun onCreate(db: SQLiteDatabase) {
+            db.execSQL("""
+            CREATE TABLE user (
+                id TEXT PRIMARY KEY,
+                email TEXT,
+                nickname TEXT,
+                password TEXT
+            )
+        """.trimIndent())
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+            db.execSQL("DROP TABLE IF EXISTS user")
+            onCreate(db)
+        }
+    }
+
+}
