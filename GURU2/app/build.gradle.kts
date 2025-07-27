183bcc07cf04fb857e0737cc2ce7457928c38beb
sonotri <ahj5021@naver.com>
2025-07-28 05:31:22 +0900

    캘린더 라이브러리 추가

--- GURU2/app/build.gradle.kts
+++ GURU2/app/build.gradle.kts
@@ -55,5 +55,6 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.google.android.gms:play-services-maps:19.2.0")
+    implementation("com.prolificinteractive:material-calendarview:1.4.3") //캘린더 양식 github import
 
 }
