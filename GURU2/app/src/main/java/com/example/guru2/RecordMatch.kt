31d94df6d11b87adbd029476d6760df45c04b427
eunkirosa <eunkirosa@swu.ac.kr>
2025-07-30 20:37:44 +0900

    직관/집관 기록 수정, 삭제 구현

---
+++ GURU2/app/src/main/java/com/example/guru2/RecordMatch.kt
@@ -0,0 +1,14 @@
+package com.example.guru2
+
+data class RecordMatch(
+    val id: Int,
+    val date: String,
+    val team1: String,
+    val team2: String,
+    val stadium: String,
+    val viewType: String,
+    val resultEmoji: String,
+    val score1: Int,
+    val score2: Int,
+    val review: String
+)
