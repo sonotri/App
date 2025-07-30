6b6cb8030f5eaca90be7060e5e6703f1ba1bca22
wogml <hanjaehee77@naver.com>
2025-07-30 12:52:23 +0900

    앱 인트로, 사진, 이름 변경

---
+++ GURU2/app/src/main/java/com/example/guru2/SplashActivity.kt
@@ -0,0 +1,24 @@
+// SplashActivity.kt
+package com.example.guru2
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
+
